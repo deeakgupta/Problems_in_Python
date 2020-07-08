f0f2201d1da2d23e71c911a515d4538abfea95c4
manish0pal <manish1999pal@gmail.com>
2020-07-08 13:56:56 +0530

    update picknumber

--- Picking Numbers.py
+++ Picking Numbers.py
@@ -4,27 +4,17 @@ def pickingNumbers(a):
     alist=[]
     while(a):
         t = a[0]
-        tl=[]
         popl=[]
-        for i in range(len(a)) :
-            
+        for i in range(len(a)) :  
             if(a[i]==t or a[i]==(t+1)):
                 popl.append(i)
+            else:
+                break
             
-        
         if(popl):
             alist.append(len(popl))
             for j in popl:
                 a.pop(0)
-        
-
-                
-            
-        
-
-        
-
-
     return max(alist)
 print(pickingNumbers([4 ,6 ,5 ,3 ,3 ,1]))
 
